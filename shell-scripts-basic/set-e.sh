6ca2bf33ac6da340375e853dd87fa9abd1f4a3ca
lokeshmeduru123 <lokeshmeduru4043@gmail.com>
2024-05-05 00:28:32 +0530

    repos

---
+++ shell-scripts-basic/set-e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+#set -e helps to exit the script if error occurs
+USERID=$(id -u)
+Failure(){
+    echo "error in $1, command $2"
+trap 'Failure ${LINENO} $BASH_COMMAND' ERR
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run the script with root access"
+    exit 1 # manually exit if error occurs
+else
+    echo "you are a super user"
+fi    
+dnf install mysqlkkk -y 
+dnf install git -y 
+ 
