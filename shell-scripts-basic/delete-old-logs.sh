68bb1d3559f8815e8d24f2fe33e1b386748fb07a
lokeshmeduru123 <lokeshmeduru4043@gmail.com>
2024-05-05 01:08:27 +0530

    repos

--- shell-scripts-basic/delete-old-logs.sh
+++ shell-scripts-basic/delete-old-logs.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 SOURCE_DIRECTORY="/var/log/"
-file=("find /var/log/ -name "*.log" -mtime -7")
 
-rm -rf $file
-Echo "deleting $file"
+if  [ -d SOURCE_DIRECTORY ]
+then
+    echo "source directory exists"
+else
+    echo "source directry not present"
+    exit 1
+fi
+
+FILES=$("find /var/log/ -name "*.log" -mtime -7")
+
+while IFS = read -r line
+do 
+    echo "Deleting the file $line"
+    rm -rf $line
+done  <<< $FILES
 
