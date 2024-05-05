7624ad3e049b436e881d4b057f18201aad36f20d
lokeshmeduru123 <lokeshmeduru4043@gmail.com>
2024-05-05 10:33:54 +0530

    repos

--- shell-scripts-basic/delete-old-logs.sh
+++ shell-scripts-basic/delete-old-logs.sh
@@ -7,3 +7,14 @@ if [ -d "$DIRECTORY" ]; then
 else 
     echo "$DIRECTORY doesn't exist"
 fi
+
+FILES=$(find $DIRECTORY -name "*.log" -mtime +9)
+
+echo "Files to delete $FILES"
+
+
+#IFS=internel field separator, helps to sepearate lines
+while IFS = read -r line
+do 
+    echo "Deleting files $line"
+done <<<$FILES
