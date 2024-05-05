9f03f9be9698bc2b83d2c68b7f5fb396db3a5611
lokeshmeduru123 <lokeshmeduru4043@gmail.com>
2024-05-05 12:23:37 +0530

    repos

---
+++ shell-scripts-basic/disk-utilisation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -i xfs)
+DISK_THRESHOLD=75
+
+while IFS= read -r line
+do 
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$FOLDER is is greater then $DISK_THRESHOLD. Current usage is :$USAGE"
+    fi
+done <<<$DISK_USAGE
